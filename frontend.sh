1cac0f778e69c755e1737cb7e57e419c47d98b0e
premsarja <premsagar@prodot.in>
2023-10-04 23:23:53 +0530

    done

--- frontend.sh
+++ frontend.sh
@@ -16,3 +16,7 @@ systemctl start nginx
 
 echo "download code into nginx path"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" 
+
+cd /usr/share/nginx/html
+rm -rf *
+# unzip /tmp/frontend.zip
