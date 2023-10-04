389cebbcfa2a65aa564dd1f1ba4a1509167a9a8d
premsarja <premsagar@prodot.in>
2023-10-04 22:55:23 +0530

    done

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,8 @@
 user_id=$(id -u)
 
 if [ $user_id -ne 0 ]; then
-  echo -e "\e[33m please perform with root user \e[0m"
+  echo -e "\e[33m please perform with root user \e[0m \N
+  example:ROOT USER"
   exit 1
 fi  
+
