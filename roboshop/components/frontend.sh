e15757fdf109e9f74c9eb1d4d995380c5ae93d72
premsarja <premsagar@prodot.in>
2023-10-27 15:46:48 +0530

    done

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,1 +1,24 @@
-echo "hi"
+#/bin/bash
+
+echo "i am frontend"
+
+USER_ID=$(id -u)
+
+if [[ $USER_ID -ne 0 ]] ; then
+   echo "please be ROOT user"
+fi
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+
