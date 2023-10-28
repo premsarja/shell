4b225aa14ed65a1b8f1274b76ec5c3b5bec1819b
premsarja <premsagar@prodot.in>
2023-10-28 17:29:54 +0530

    done

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -34,8 +34,8 @@ STATUS $?
 echo -ne " installing the component " 
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
 cd /home/roboshop
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> /tmp/catalogue.log
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
-npm install
+npm install &>> /tmp/catalogue.log
 STATUS $?
