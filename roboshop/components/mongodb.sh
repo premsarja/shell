3a822eb1cb196e68eb1fe22db7ab123e321e48aa
premsarja <premsagar@prodot.in>
2023-10-28 13:15:05 +0530

    done

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -27,7 +27,7 @@ echo -ne "\e[33m downloading the mongoDb ......! \e[0m; "
 yum install -y mongodb-org
 systemctl enable mongod
 systemctl start mongod
-STATUS 
+STATUS $?
 
 echo -ne "\e[33m update the < bind IP > ......! \e[0m; "
 sed -ie "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
@@ -39,5 +39,13 @@ systemctl start mongod
 STATUS $?
 
 
+echo -ne "\e[33m download and install schema : "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
+STATUS $?
 
 
