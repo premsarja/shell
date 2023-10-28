f248c485a1f83cd48b0eac6d98766de3d1f34137
premsarja <premsagar@prodot.in>
2023-10-28 13:22:09 +0530

    done

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -43,6 +43,9 @@ echo -ne "\e[33m download and install schema \e[0m: "
 curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 cd /tmp
 unzip -o mongodb.zip &>> /tmp/mongod.log
+STATUS $?
+
+echo -ne "\e[33m installing  schema \e[0m: "
 cd mongodb-main
 mongo < catalogue.js
 mongo < users.js
