b80048030a6c6225eb82a9954cb6dbc30bfc1e6e
premsarja <premsagar@prodot.in>
2023-10-28 12:20:26 +0530

    done

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -35,10 +35,10 @@ echo -n "downloading the component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/frontend.log
 STATUS $?
 
- cd /usr/share/nginx/html
- rm -rf *
- unzip /tmp/frontend.zip
- mv frontend-main/* .
- mv static/* .
- rm -rf frontend-main README.md
- mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#  cd /usr/share/nginx/html
+#  rm -rf *
+#  unzip /tmp/frontend.zip
+#  mv frontend-main/* .
+#  mv static/* .
+#  rm -rf frontend-main README.md
+#  mv localhost.conf /etc/nginx/default.d/roboshop.conf
