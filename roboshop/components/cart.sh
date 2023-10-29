9dd05438e9589028ecaf47c486c42f00e842c48a
premsarja <premsagar@prodot.in>
2023-10-29 14:21:36 +0530

    done

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,71 +1,59 @@
-#!/bin/bash 
+#!/bin/bash
 
 echo "i am cart"
-
 
 USER_ID=$(id -u)
 
 if [[ $USER_ID -ne 0 ]] ; then
-   echo -e "\e[31m please be ROOT user \e[0m  \n \t EXAMPLE: sudo <filename> "
+   echo -e "\e[31m Please run this script as the ROOT user. \e[0m \n \t EXAMPLE: sudo <filename>"
    exit 1
 fi
 
 STATUS() {
     if [[ $? -eq 0 ]]; then
-    echo -e "\e[32m sucees \e[0m" 
-  else
-    echo -e "\e[33m failed \e[0m" 
-    exit 2
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[33m Failed \e[0m"
+        exit 2
     fi
 }
 
-echo -ne "\e[33m installing the nodejs component \e[0m ; "
+echo -ne "\e[33m Installing the nodejs component \e[0m ; "
 yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
-yum install nodejs -y 
-STATUS $?
+yum install nodejs -y
+STATUS
 
-echo -ne "\e[33m creating roboshop user \e[0m ; "
+echo -ne "\e[33m Creating roboshop user \e[0m ; "
 ID=roboshop
 id=${ID}
-  id ${id}  
-  if [ $? -ne 0 ] ; then 
-      echo -n "Creating Application User Account :"
-      useradd roboshop
-      STATUS $? 
-  fi    
-STATUS $?
+id ${id}
+if [ $? -ne 0 ] ; then
+    echo -n "Creating Application User Account: "
+    useradd roboshop
+    STATUS
+fi
 
-echo -ne " installing the component " 
+echo -n "Installing the component: "
 cd /home/roboshop
 curl -s -L -o /tmp/cart.zip "https://github.com/stans-robot-project/cart/archive/main.zip"
 unzip -o /tmp/cart.zip &>> /tmp/cart.log
-STATUS $?
+STATUS
 
-echo -n "moving component: "
-mv cart-main cart
+echo -n "Moving component: "
+mv /home/roboshop/cart-main /home/roboshop/cart
+cd /home/roboshop/cart
 chown -R roboshop:roboshop /home/roboshop/cart
 npm install
-STATUS $?
+STATUS
 
+echo -n "Updating the file: "
+sed -i -e 's/REDIS_ENDPOINT/172.31.27.233/' -e 's/CATALOGUE_ENDPOINT/172.31.16.108/' /home/roboshop/cart/systemd.service
+STATUS
 
-# echo -n "adding the COMPONENTs inside the ${user} user account:"
-# curl -s -L -o /tmp/cart.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-# cd /home/roboshop
-# unzip -o /tmp/${COMPONENT}.zip
-# mv ${COMPONENT}-main cart
-# npm install
-# sed -ie "s/REDIS_ENDPOINT/redis.roboshop-internal/g" /home/roboshop/${COMPONENT}/systemd.servicee
-# sed -ie "s/CATALOGUE_ENDPOINT/catalogue.roboshop-internal/g" /home/roboshop/${COMPONENT}/systemd.servicee
-# statusfunction $?
-
-echo -n "updating the file:  "
-sed -ie 's/REDIS_ENDPOINT/172.31.27.233/' /home/roboshop/cart/systemd.service
-sed -ie 's/CATALOGUE_ENDPOINT/172.31.16.108/' /home/roboshop/cart/systemd.service
-STATUS $?
-
-echo -n "starting the systemfile: "
+echo -n "Starting the service: "
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 systemctl daemon-reload
-systemctl start user
-systemctl status user -l
-STATUS $?
+systemctl start cart
+systemctl enable cart
+systemctl status cart -l
+STATUS
