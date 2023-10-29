7bf96bfb368a4118827eeeedbf9afcc6e618c382
premsarja <premsagar@prodot.in>
2023-10-29 12:36:35 +0530

    done

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -26,7 +26,7 @@ STATUS $?
 
 echo -ne "\e[33m creating roboshop user \e[0m ; "
 id=${ID}
- if [[ $id -ne 0 ]]; then
+ if [[ $id -eq 0 ]]; then
   useradd roboshop
   echo "the PWD is : ${PWD}"
  fi
