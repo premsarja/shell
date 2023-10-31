259071e946423b493f0b2ed1e34948dd80a9b98d
premsarja <premsagar@prodot.in>
2023-10-31 13:34:10 +0530

    done

--- roboshop/launchEc2.sh
+++ roboshop/launchEc2.sh
@@ -11,7 +11,7 @@ INSTANCE_TYPE="t2.micro"
 SECURITY_GROUP="sg-071baaff364d61305"
 COMPONENT=$1
 if [ -z $1 ]; then
-  echo -e "\e[33m COMPONENT name is needed\e[0m"
+  echo -e "\e[31m COMPONENT name is needed\e[0m"
   echo -e "\e[35m ex;usage $bash launch-ec2.sh \e[0m"
   exit 1
 fi  
@@ -22,4 +22,4 @@ fi
 # BU,ENV,APP:COST_CENTER
 
 # aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type ${INSTANCE_TYPE} --security-group-ids ${SECURITY_GROUP} --tag-specification "ResourceType=instance,Tags=[{key=Name,Value=1}]"
-aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --instance-type t3.micro --security-group-ids sg-071baaff364d61305  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=lab}]"
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids sg-071baaff364d61305  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
