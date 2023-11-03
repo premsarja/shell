0f61e810f5f7a25fc0ecf152316856fc5ee12b5c
premsarja <premsagar@prodot.in>
2023-11-03 15:24:00 +0530

    done

--- roboshop/ll.sh
+++ roboshop/ll.sh
@@ -3,3 +3,4 @@
 aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --count 1 --instance-type t2.micro  --security-group-ids sg-00f2ddd66e34b1879
 HOSTED_ID=Z01927153H3BLSGWLBLEA
  
+PRIVATE_IP=$(aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --count 1 --instance-type t2.micro  --security-group-ids sg-00f2ddd66e34b1879| jq .Instances[].PrivateIpAddress) 
