cdd7d3a62986f12789e3b6329064dc1cc2d3fa36
premsarja <premsagar@prodot.in>
2023-11-03 18:15:50 +0530

    done

--- roboshop/ll.sh
+++ roboshop/ll.sh
@@ -4,7 +4,6 @@
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')
 INSTANCE_TYPE="t2.micro"
 SECURITY_GROUP=$(aws ec2 describe-security-groups --filters Name=group-name,Values=default | jq '.SecurityGroups[].GroupName'| sed -e 's/"//g')
-HOSTEDZONE_ID="Z01927153H3BLSGWLBLEA"
 COMPONENT=$1
 env="dev"
 if [ -z $1 ]; then
