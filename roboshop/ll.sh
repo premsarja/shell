348af58391a9491bf82bcfffb08c5225b9ccb5eb
premsarja <premsagar@prodot.in>
2023-11-04 10:59:09 +0530

    done

--- roboshop/ll.sh
+++ roboshop/ll.sh
@@ -2,10 +2,11 @@
 
 # aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --count 1 --instance-type t2.micro  --security-group-ids sg-00f2ddd66e34b1879
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')
-INSTANCE_TYPE="t2.micro"
-SECURITY_GROUP=$(aws ec2 describe-security-groups --filters Name=group-name,Values=default | jq '.SecurityGroups[].GroupName'| sed -e 's/"//g')
+INSTANCE_TYPE="t2.micro"2
+SECURITY_GROUP=$(aws ec2 describe-security-groups --filters Name=group-name,Values=sg-071baaff364d61305 | jq '.SecurityGroups[].GroupName'| sed -e 's/"//g')
 COMPONENT=$1
 ENV="DEV"
+
 if [ -z $1 ]; then
   echo -e "\e[31m COMPONENT name is needed\e[0m"
   echo -e "\e[35m ex;usage $bash launch-ec2.sh \e[0m"
